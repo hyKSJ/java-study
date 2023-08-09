8f803b471c468dd3cc69b1a39da6ccabe3241f6c
hyKSJ <tkdwns9494@gmail.com>
2023-08-09 12:50:20 +0900

    필드/변수 finished

--- chapter03/src/main/java/chapter03/Goods2.java
+++ chapter03/src/main/java/chapter03/Goods2.java
@@ -2,7 +2,7 @@ package chapter03;
 
 public class Goods2 {
 	public String name;     // 모든 접근 가능
-	protected int price = 10;    // 같은 패키지 or **자식클래스만 접근 가능
+	protected int price;    // 같은 패키지 or **자식클래스만 접근 가능
 	int countStock; // 같은 패키지(디폴트)에서만 접근 가능
 	private int countSold;  // 클래스 내부에서만 접근 가능
 }
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -4,13 +4,24 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Goods camera = new Goods();
-		camera.name = "nikon";
-		camera.price = 400000;
-		camera.countStock = 30;
-		camera.countSold = 50;
+		Goods camera1 = new Goods();
+		Goods camera2 = new Goods();
+		System.out.println(Goods.countOfGoods);
+//		camera.name = "nikon";
+		camera.setName("nikon");
+//		camera.price = 400000;
+		camera.setPrice(400000);
+//		camera.countStock = 30;
+		camera.setCountStock(30);
+//		camera.countSold = 50;
+		camera.setCountSold(50);
+		
+		// 정보은닉(데이터보호)
+//		camera.setPrice(-1);
+		
 
-		System.out.println("상품이름 : " + camera.name + ", 가격 : " + camera.price + ", 재고개수 : " + camera.countStock
-				+ ", 팔린 개수 : " + camera.countSold);
+		System.out.println("상품이름 : " + camera.getName() + ", 가격 : " + camera.getPrice() + ", 재고개수 : " + camera.getCountStock()
+				+ ", 팔린 개수 : " + camera.getCountSold());
 	}
 
 }
