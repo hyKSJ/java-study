c45536d4b1c04ab3be2fb178b93d26b19057eaf7
hyKSJ <tkdwns9494@gmail.com>
2023-08-10 15:56:42 +0900

    exception

--- chapter03/src/main/java/com/poscodx/paint/main/Main.java
+++ chapter03/src/main/java/com/poscodx/paint/main/Main.java
@@ -1,4 +1,16 @@
-package paint;
+package com.poscodx.paint.main;
+
+import com.poscodx.paint.i.Drawable;
+import com.poscodx.paint.point.ColorPoint;
+import com.poscodx.paint.point.Point;
+
+//import com.poscodx.paint.shape.Circle;
+//import com.poscodx.paint.shape.Rect;
+//import com.poscodx.paint.shape.Shape;
+//import com.poscodx.paint.shape.Triangle;
+
+import com.poscodx.paint.shape.*;
+import com.poscodx.paint.text.GraphicText;
 
 public class Main {
 
@@ -17,7 +29,7 @@ public class Main {
 		point2.setX(20);
 		point2.setY(20);
 		
-		// 다운캐스팅 명시적으로 표현
+		// 다운캐스팅 명시적으로 표현dsa
 //		ColorPoint p = (ColorPoint) point2;
 		
 		((ColorPoint)point2).setColor("red");
@@ -34,6 +46,21 @@ public class Main {
 		
 		draw(new GraphicText("Hello World"));
 		
+		// instanceof 연산자 test
+		
+		System.out.println(circle instanceof Circle);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Object);
+		
+		// 오류: 연산자 우측항이 클래스인 경우,
+		//      레퍼런스 하고 있는 클래스 타입의 hierachy 상의 하위와 상위만
+		//      instanceof 연산자를 사용할 수 있다.
+//		System.out.println(circle instanceof Rect);
+
+		// 연산자 우측항이 인터페이스인 경우,
+		// hierachy 상관없이(애초에 hierachy 관계도 아님) instanceof 연산자를 사용할 수 있다.
+		System.out.println(circle instanceof Drawable);
+		System.out.println(circle instanceof Runnable);
 	}
 	
 	public static void draw(Drawable drawable) {
