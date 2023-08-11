0110c25130dbc78e636f54a908e668b20012966c
hyKSJ <tkdwns9494@gmail.com>
2023-08-11 14:50:04 +0900

    String

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,25 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		
+		// "Hello"
+		System.out.println("\"Hello\"");
+		
+		// \t: tab
+		// \r: carrige return
+		// \n: newline
+		// \b: bell
+		
+		// '
+		System.out.println('\'');
+		
+//		String path = "/poscodx2023/DoWork/HelloWorld2.java";
+
+	}
+
+}
---
+++ chapter04/src/main/java/chapter04/StringTest03.java
@@ -0,0 +1,63 @@
+package chapter04;
+
+public class StringTest03 {
+
+	public static void main(String[] args) {
+//		String s1 = "Hello" + "World" + "Java" + 17;
+		StringBuffer sb = new StringBuffer("Hello");
+//		sb.append("World");
+//		sb.append("Java");
+//		sb.append(17);
+//		String s1 = sb.toString();
+
+		String s1 = sb.append("World").append("Java").append(17).toString();
+
+		System.out.println(s1);
+		
+		String s2 = "";
+		for(int i = 0; i < 1000000; i++) {
+//			s2 += i;
+//			s2 = new StringBuffer(s2).append(i).toString();
+		}
+		
+		StringBuffer sb2 = new StringBuffer("");
+		for (int i = 0; i < 1000000; i++) {
+			sb2.append(i);
+		}
+		
+		String s3 = sb2.toString();
+		
+//		System.out.println(s3);
+		
+		// String method들 ..
+		String s4 = "aBcABCabcAbc";
+		
+		System.out.println(s4.length());
+		System.out.println(s4.charAt(2));
+		System.out.println(s4.indexOf("abc"));
+		System.out.println(s4.indexOf("abc",7));
+		System.out.println(s4.substring(3));
+		System.out.println(s4.substring(3,5));
+	
+		String s5 = "   ab   cd   ";
+		String s6 = "efg,hij,klm,nop,qrs";
+		
+		String s7 = s5.concat(s6);
+		System.out.println(s7);
+		
+		System.out.println(s5.trim());
+		System.out.println(s5.replaceAll(" ", ""));
+		
+		String[] tokens = s6.split(",");
+		for (String s : tokens) {
+			System.out.println(s);
+		}
+		
+		String[] tokens2 = s6.split(" ");
+		for (String s : tokens2) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
