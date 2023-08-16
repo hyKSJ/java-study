a54b8121795e01308d366da7ba6ac31ba872c616
hyKSJ <tkdwns9494@gmail.com>
2023-08-16 11:52:35 +0900

    collection framework

---
+++ chapter04/src/main/java/collection/HashSetTest02.java
@@ -0,0 +1,17 @@
+package collection;
+
+public class HashSetTest02 {
+
+	public static void main(String[] args) {
+		Set<Gugudan> set = new HashSet<>();
+
+		set.add(new Gugudan(2, 3));
+		set.add(new Gugudan(9, 9));
+		set.add(new Gugudan(3, 2));
+		
+		for(Gugudan g : set) {
+			System.out.println(g);
+		}
+	}
+
+}
